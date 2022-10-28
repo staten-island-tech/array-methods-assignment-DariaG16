303bd74597398a8bad2b5dd1d35404cbb522ede6
DariaG16 <dariagolitsyna16@gmail.com>
2022-10-28 13:59:48 -0400

    dsfa

--- index.js
+++ index.js
@@ -4,6 +4,37 @@ cookies.forEach((cookie) ==> {
 
 const result = cookies.filter((cookie) => cookie.type = "bomber");
 console.log(result);
-arr:cookies = [
+const cookies = [
+    {
+        name:"Gingerbrave",
+        skill:"charge",
+        rarity:"common",
+        usefulIn:["arena","some other gamemode"]
+    },
+    {
+        name:"Cotton Cookie",
+        skill:"Support",
+        rarity:"Epic",
+        usefulIn:["Arena","World Exploration, Any"]
+    },
+    {
+        name:"gingerbrave",
+        skill:"charge",
+        rarity:"common",
+        usefulIn:["arena","some other gamemode"]
+    },
+    {
+        name:"gingerbrave",
+        skill:"charge",
+        rarity:"common",
+        usefulIn:["arena","some other gamemode"]
+    },
+    {
+        name:"gingerbrave",
+        skill:"charge",
+        rarity:"common",
+        usefulIn:["arena","some other gamemode"]
+    }
+
 
 ]
