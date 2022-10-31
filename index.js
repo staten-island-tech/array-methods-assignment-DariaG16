b452e73d0a8cef243d964c38802f5ecd685c8539
DariaG16 <dariagolitsyna16@gmail.com>
2022-10-31 14:01:54 -0400

    asdlgfjdshfgfdsgadsfasg

--- index.js
+++ index.js
@@ -1,40 +1,39 @@
-cookies.forEach((cookie) ==> {
-    console.log(cookie.name);
-});
-
-const result = cookies.filter((cookie) => cookie.type = "bomber");
-console.log(result);
 const cookies = [
-    {
-        name:"Gingerbrave",
-        skill:"charge",
-        rarity:"common",
-        usefulIn:["arena","some other gamemode"]
-    },
-    {
-        name:"Cotton Cookie",
-        skill:"Support",
-        rarity:"Epic",
-        usefulIn:["Arena","World Exploration, Any"]
-    },
-    {
-        name:"gingerbrave",
-        skill:"charge",
-        rarity:"common",
-        usefulIn:["arena","some other gamemode"]
-    },
-    {
-        name:"gingerbrave",
-        skill:"charge",
-        rarity:"common",
-        usefulIn:["arena","some other gamemode"]
-    },
-    {
-        name:"gingerbrave",
-        skill:"charge",
-        rarity:"common",
-        usefulIn:["arena","some other gamemode"]
-    }
-
-
-]
+  {
+    name: "Gingerbrave",
+    skill: "charge",
+    rarity: "common",
+    usefulIn: ["arena", "master mode", "odyssey"],
+  },
+  {
+    name: "Cotton Cookie",
+    skill: "support",
+    rarity: "epic",
+    usefulIn: ["arena", "world exploration", "master mode", "odyssey"],
+  },
+  {
+    name: "Financier Cookie",
+    skill: "defense",
+    rarity: "epic",
+    usefulIn: ["arena", "master mode", "odyssey"],
+  },
+  {
+    name: "Caramel Arrow Cookie",
+    skill: "ranged",
+    rarity: "epic",
+    usefulIn: ["arena", "world exploration", "master mode", "odyssey"],
+  },
+  {
+    name: "Black Pearl Cookie",
+    skill: "ambush",
+    rarity: "legendary",
+    usefulIn: ["arena", "world exploration", "master mode"],
+  },
+];
+cookies.forEach((cookie) => console.log(cookie.name));
+cookies.forEach((cookie) => {
+  cookie.usefulIn.forEach(cookie.usefulIn) > console.log(cookie.usefulIn);
+});
+console.log(cookies);
+const result = cookies.filter((cookie) => (cookie.skill = "charge"));
+console.log(result);
