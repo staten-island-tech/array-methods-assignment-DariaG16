d6a429b3f57a839fc6dfa8961877e583a356a8c6
DariaG16 <dariagolitsyna16@gmail.com>
2022-11-01 23:57:31 -0400

    asdf

--- index.js
+++ index.js
@@ -33,11 +33,22 @@ const cookies = [
 cookies.forEach((cookie) => console.log(cookie.name)); //logs every cookies name
 //cookies.forEach((cookie) => console.log(cookie.usefulIn)); //logs every cookies skill?
 
- cookies.forEach((cookie) => {
-  cookie.usefulIn.forEach((cookies, cookie.usefulIn),
-  console.log(cookie.usefulIn));//the code mr whalen gave us
- 
+cookies.forEach((cookie) => {
+  cookies.usefulIn.forEach((cookie) => {
+    console.log(forEach);
+  });
+}); //the code mr whalen gave us
 
 const result = cookies.filter((cookie) => cookie.skill === "ranged"); //filters the cookies based on their skills
 console.log(result);
 cookies.forEach((cookies) => console.log(cookies.usefulIn));
+
+Row1.forEach((Character) => {
+  console.log(Character.name);
+  console.log(Character.ability);
+  Character.numbers.forEach((numbers) => {
+    console.log(numbers);
+  });
+});
+
+displayStats();
